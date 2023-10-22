8b06b107484e05a4b0e2a17d87a3463ae4e00465
MarketaGi <marketa.ginovska@gmail.com>
2023-10-22 22:16:27 +0200

    First version of project "Text Analyzator"

--- skript1.py
+++ skript1.py
@@ -1,3 +1,33 @@
-print('Ahoj studenti Python Akademie')
-print("pridala jsem zmenu")
-print("pridala jsem druhou zmenu")
+def split_words_with_letters_and_numbers(text):
+    # Initialize an empty list to store the split words
+    split_words = []
+
+    # Initialize empty strings to store the letters and numbers
+    letters = ""
+    numbers = ""
+
+    # Iterate through each character in the text
+    for char in text:
+        if char.isalpha():
+            letters += char
+        elif char.isdigit():
+            numbers += char
+        else:
+            # If a non-letter, non-digit character is encountered, add the previous word to the list
+            if letters:
+                split_words.append((letters, numbers))
+                letters = ""
+                numbers = ""
+
+    # Add the last word if it exists
+    if letters:
+        split_words.append((letters, numbers))
+
+    return split_words
+
+# Example usage:
+text = "This is a text with words like word123 and another word456."
+word_splits = split_words_with_letters_and_numbers(text)
+
+for idx, (letters_part, numbers_part) in enumerate(word_splits, start=1):
+    print(f"Word {idx} - Letters Part: {letters_part}, Numbers Part: {numbers_part}")
